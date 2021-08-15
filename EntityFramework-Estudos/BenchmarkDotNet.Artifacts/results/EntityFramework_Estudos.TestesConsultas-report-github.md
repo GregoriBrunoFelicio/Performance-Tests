b56bbbf3a602011f69984b0b884ef8e14c0f652a
gregori bruno felicio <gregory.wow@hotmail.com>
2021-08-15 13:58:41 -0300

    testes joins

--- EntityFramework-Estudos/BenchmarkDotNet.Artifacts/results/EntityFramework_Estudos.TestesConsultas-report-github.md
+++ EntityFramework-Estudos/BenchmarkDotNet.Artifacts/results/EntityFramework_Estudos.TestesConsultas-report-github.md
@@ -1,14 +1,15 @@
 ``` ini
 
-BenchmarkDotNet=v0.13.0, OS=Windows 10.0.18363.1679 (1909/November2019Update/19H2)
+BenchmarkDotNet=v0.13.0, OS=Windows 10.0.18363.1734 (1909/November2019Update/19H2)
 Intel Core i7-8700 CPU 3.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
-.NET SDK=5.0.301
-  [Host]     : .NET Core 3.1.16 (CoreCLR 4.700.21.26205, CoreFX 4.700.21.26205), X64 RyuJIT
-  DefaultJob : .NET Core 3.1.16 (CoreCLR 4.700.21.26205, CoreFX 4.700.21.26205), X64 RyuJIT
+.NET SDK=5.0.302
+  [Host]     : .NET Core 3.1.17 (CoreCLR 4.700.21.31506, CoreFX 4.700.21.31502), X64 RyuJIT
+  DefaultJob : .NET Core 3.1.17 (CoreCLR 4.700.21.31506, CoreFX 4.700.21.31502), X64 RyuJIT
 
 
 ```
-|                       Method |     Mean |     Error |    StdDev |    Gen 0 |    Gen 1 | Gen 2 | Allocated |
-|----------------------------- |---------:|----------:|----------:|---------:|---------:|------:|----------:|
-| ObterUsuariosSemAsNoTracking | 3.761 ms | 0.0284 ms | 0.0222 ms | 257.8125 | 117.1875 |     - |  1,609 KB |
-| ObterUsuariosComAsNoTracking | 2.238 ms | 0.0274 ms | 0.0229 ms | 109.3750 |  31.2500 |     - |    675 KB |
+|         Method |      Mean |     Error |    StdDev |    Gen 0 |    Gen 1 |    Gen 2 | Allocated |
+|--------------- |----------:|----------:|----------:|---------:|---------:|---------:|----------:|
+|    JoinComLinq |  7.987 ms | 0.0793 ms | 0.0703 ms | 453.1250 | 156.2500 |  62.5000 |      3 MB |
+|    JoinSemLinq |  7.968 ms | 0.0611 ms | 0.0541 ms | 453.1250 | 156.2500 |  62.5000 |      3 MB |
+| JoinComInclude | 18.259 ms | 0.3594 ms | 0.4798 ms | 875.0000 | 375.0000 | 156.2500 |      5 MB |
--- EntityFramework-Estudos/Program.cs
+++ EntityFramework-Estudos/Program.cs
@@ -29,6 +29,9 @@ namespace EntityFramework_Estudos
 
             //var teste = new TestesConsultas();
 
+            //var a = teste.JoinComLinq();
+            //var b = teste.JoinSemLinq();
+            //var c = teste.JoinComInclude();
 
             BenchmarkRunner.Run<TestesConsultas>();
         }
@@ -37,20 +40,99 @@ namespace EntityFramework_Estudos
     [MemoryDiagnoser]
     public class TestesConsultas
     {
+        //[Benchmark]
+        //public List<Usuario> ObterUsuariosSemAsNoTracking()
+        //{
+        //    using var context = new Context();
+        //    var usuarios = context.Usuario.ToList();
+        //    return usuarios;
+        //}
+
+        //[Benchmark]
+        //public List<Usuario> ObterUsuariosComAsNoTracking()
+        //{
+        //    using var context = new Context();
+        //    var usuarios = context.Usuario.AsNoTracking().ToList();
+        //    return usuarios;
+        //}
+
+        //[Benchmark]
+        //public IList<Usuario> PaginacaoComAsNoTracking()
+        //{
+        //    var pagina = 3;
+        //    var tamanhoPagina = 10;
+        //    using var context = new Context();
+        //    var usuarios = context.Usuario.AsNoTracking().Skip((pagina - 1) * tamanhoPagina).Take(tamanhoPagina).AsQueryable();
+        //    return usuarios.ToList();
+        //}
+
+        //[Benchmark]
+        //public List<Usuario> PaginacaoSemAsNoTracking()
+        //{
+        //    using var context = new Context();
+        //    var usuarios = context.Usuario.AsNoTracking().ToList();
+        //    return usuarios;
+        //}
+
+        //[Benchmark]
+        //public async Task<Usuario> FirstOrDefault()
+        //{
+        //    await using var context = new Context();
+        //    var usuario = await context.Usuario.AsNoTracking().FirstOrDefaultAsync(x => x.Id == 103);
+        //    return usuario;
+        //}
+
+        //[Benchmark]
+        //public async Task<Usuario> SingleOrDefault()
+        //{
+        //    await using var context = new Context();
+        //    var usuario = await context.Usuario.AsNoTracking().SingleOrDefaultAsync(x => x.Id == 103);
+        //    return usuario;
+        //}
+
         [Benchmark]
-        public List<Usuario> ObterUsuariosSemAsNoTracking()
+        public List<Usuario> JoinComLinq()
         {
             using var context = new Context();
-            var usuarios = context.Usuario.ToList();
-            return usuarios;
+            var usuario = context.Usuario.Join(context.Endereco, u => u.EnderecoId, e => e.Id, (u, e) => new { u, e })
+                .Join(context.Cargo, @t => @t.u.EnderecoId, c => c.Id,
+                    (@t, c) => new Usuario
+                    {
+                        Nome = @t.u.Nome,
+                        Cargo = c,
+                        Endereco = @t.e,
+                        Sobrenome = @t.u.Sobrenome,
+                        Telefone = @t.u.Telefone
+                    });
+
+            return usuario.ToList();
         }
 
         [Benchmark]
-        public List<Usuario> ObterUsuariosComAsNoTracking()
+        public List<Usuario> JoinSemLinq()
         {
             using var context = new Context();
-            var usuarios = context.Usuario.AsNoTracking().ToList();
-            return usuarios;
+            var usuario = from u in context.Usuario
+                          join e in context.Endereco on u.EnderecoId equals e.Id
+                          join c in context.Cargo on u.EnderecoId equals c.Id
+                          select new Usuario
+                          {
+                              Nome = u.Nome,
+                              Cargo = c,
+                              Endereco = e,
+                              Sobrenome = u.Sobrenome,
+                              Telefone = u.Telefone
+                          };
+
+            return usuario.ToList();
+        }
+
+        [Benchmark]
+        public List<Usuario> JoinComInclude()
+        {
+            using var context = new Context();
+            var usuario = context.Usuario.Include(x => x.Cargo).Include(x => x.Endereco);
+            return usuario.ToList();
         }
     }
 }
