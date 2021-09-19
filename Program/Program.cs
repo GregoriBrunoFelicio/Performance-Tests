e5a251d081fd432816c22cc87d3c7c75a8a52be0
gregori bruno felicio <gregory.wow@hotmail.com>
2021-09-19 00:30:15 -0300

    testes unsafe

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,6 +1,5 @@
 ﻿#nullable enable
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
 using System;
 using System.IO;
 
@@ -10,8 +9,21 @@ namespace Program
     {
         public static void Main()
         {
-            BenchmarkRunner.Run<Teste>();
+            unsafe
+            {
+                //BenchmarkRunner.Run<Teste>();
+                var n1 = 10;
+                var n2 = 10;
+
+                var n3 = &n1;
+                var n4 = &n2;
+
+                var a = SumUnsafe(n3, n4);
+                Console.WriteLine(a);
+            }
         }
+
+        static unsafe int SumUnsafe(int* n1, int* n2) => (int)(&n1 + (ulong)&n2);
     }
 
     public class Teste
